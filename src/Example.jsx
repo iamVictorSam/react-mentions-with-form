919184b7b0cffdeb5058d140718fb9ddc705cf2e
Sam Victor <iamvictorsam@gmail.com>
2023-03-17 07:50:45 +0100

    Using new file

---
+++ src/Example.jsx
@@ -0,0 +1,7 @@
+import { useState, useCallback, useEffect } from "react";
+import { Mention, MentionsInput } from "react-mentions";
+import style from "./style.module.css";
+
+const MentionForm = () => {
+  return;
+};
